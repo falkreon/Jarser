68c89fd94bb00d918a4ef959189a9101410c771b
Falkreon <falkreon@gmail.com>
2021-04-28 21:49:01 -0500

    Completely untested full-file parsing

--- src/main/java/blue/endless/jarser/syntax/JarserJarser.java
+++ src/main/java/blue/endless/jarser/syntax/JarserJarser.java
@@ -13,12 +13,14 @@ public class JarserJarser {
 		Syntax.Builder builder = Syntax.builder();
 		//Lexer lexer = new Lexer();
 		
+		builder.addLexerRule("line_end_comment", "\\/\\/[^\n]*\n");
 		builder.addLexerRule("quoted_string", "\"(?:[^\"\\\\]*(?:\\\\.)?)*\"");
 		builder.addLexerRule("regexp", "\\/(?<value>(?:[^\\/\\\\]*(?:\\\\.)?)+)\\/"); // \/(?<value>(?:[^\/\\]*(?:\\.)?)+)\/
-		builder.addLexerRule("equals_operator", "=");
+		//builder.addLexerRule("equals_operator", "=");
+		builder.addLexerRule("colon_operator", ":");
 		builder.addLexerRule("repetition_operator", "[\\*\\?\\+]");
 		builder.addLexerRule("alternatives_operator", "\\|");
-		builder.addLexerRule("regular_expression", "/^((?:(?:[^?+*{}()[\\]\\\\|]+|\\\\.|\\[(?:\\^?\\\\.|\\^[^\\\\]|[^\\\\^])(?:[^\\]\\\\]+|\\\\.)*\\]|\\((?:\\?[:=!]|\\?<[=!]|\\?>)?(?1)??\\)|\\(\\?(?:R|[+-]?\\d+)\\))(?:(?:[?+*]|\\{\\d+(?:,\\d*)?\\})[?+]?)?|\\|)*)$/");
+		builder.addLexerRule("rule_end_operator", ";");
 		builder.addLexerRule("token", "[a-zA-Z_]+[a-zA-Z0-9_]*");
 		builder.addLexerRule("parens", "[\\(\\)]");
 		builder.addLexerRule("whitespace", "\\s+");
@@ -39,7 +41,6 @@ public class JarserJarser {
 				);
 		builder.add(alternativesRule);
 		
-		//TODO: what the hell
 		ProductionRule groupingRule = SequenceProductionRule.of("grouping",
 				ElementProductionRule.matchLiteral("("),
 				RepetitionProductionRule.with(
@@ -54,33 +55,40 @@ public class JarserJarser {
 				);
 		builder.add(groupingRule);
 		
+		/*
 		ProductionRule tokenProductionRule = SequenceProductionRule.of("tokenRule",
 				AlternativesProductionRule.of("nameAlternatives",
 						ElementProductionRule.matchProduction("token"),
 						ElementProductionRule.matchProduction("quoted_string")
 				),
-				ElementProductionRule.matchLiteral("="),
+				ElementProductionRule.matchLiteral(":"),
 				ElementProductionRule.matchProduction("token_keyword"),
 				ElementProductionRule.matchProduction("quoted_string")
-				);
+				);*/
 		
 		ProductionRule productionProductionRule = SequenceProductionRule.of("rule",
 				AlternativesProductionRule.of("nameAlternatives",
 						ElementProductionRule.matchProduction("token"),
 						ElementProductionRule.matchProduction("quoted_string")
 				),
-				ElementProductionRule.matchLiteral("="),
-				RepetitionProductionRule.with(
-						AlternativesProductionRule.of("repetitionAlternatives",
-								ElementProductionRule.matchProduction("token"),
-								ElementProductionRule.matchProduction("quoted_string"),
-								ElementProductionRule.matchProduction("repetition"),
-								ElementProductionRule.matchProduction("alternatives"),
-								ElementProductionRule.matchProduction("grouping")
-								),
-						RepetitionProductionRule.RepetitionType.ONE_OR_MORE
-					)
-				);
+				ElementProductionRule.matchLiteral(":"),
+				AlternativesProductionRule.of("tokenOrProduction",
+					//Production
+					RepetitionProductionRule.with(
+							AlternativesProductionRule.of("repetitionAlternatives",
+									ElementProductionRule.matchProduction("token"),
+									ElementProductionRule.matchProduction("quoted_string"),
+									ElementProductionRule.matchProduction("repetition"),
+									ElementProductionRule.matchProduction("alternatives"),
+									ElementProductionRule.matchProduction("grouping")
+									),
+							RepetitionProductionRule.RepetitionType.ONE_OR_MORE
+					),
+					//Token
+					ElementProductionRule.matchProduction("regexp")
+				),
+				ElementProductionRule.matchLiteral(";")
+			);
 		builder.add(productionProductionRule);
 		
 		return builder.build();
@@ -179,5 +187,57 @@ public class JarserJarser {
 		default:
 			throw new SyntaxException("Expected part of a production rule, found '"+prod.value()+"'", prod);
 		}
+	}
+	
+	public static Syntax makeSyntax(String s) throws SyntaxException {
+		Jarser jarser = new Jarser(JARSER_SYNTAX);
+		jarser.startMatching(s);
+		List<Production> ast = jarser.applyAll();
+		
+		Syntax.Builder builder = Syntax.builder();
+		for(Production p : ast) {
+			if (p.getName().equals("rule")) {
+				/* 
+				 * There should be 5 kinds of rulePieces: name, colon_operator, [stuff]+, semicolon.
+				 * The first element will be name. Second is colon. Last is semicolon. Everything else is the meat, the Stuff inside the rule.
+				 */
+				List<Production> rulePieces = p.getChildren();
+				//String ruleName = rulePieces.get(0).value().toString();
+				Production ruleNameToken  = rulePieces.get(0);
+				String ruleName;
+				if (ruleNameToken.getName().equals("quoted_string")) {
+					ruleName = ruleNameToken.value().toString().substring(1, ruleNameToken.value().length()-1); //TODO: use value named-capture group
+				} else if (ruleNameToken.getName().equals("token")) {
+					ruleName = ruleNameToken.value().toString();
+				} else {
+					throw new SyntaxException("Expected rule name, got "+ruleNameToken.getName());
+				}
+				
+				ArrayList<Production> stuff = new ArrayList<>();
+				for(int i=2; i<rulePieces.size()-1; i++) {
+					stuff.add(rulePieces.get(i));
+				}
+				
+				if (stuff.size()==1 && stuff.get(0).getName().equals("regexp")) {
+					//Token / Terminal / Lexer rule
+					if (stuff.get(0) instanceof Token) {
+						String regexValue = ((Token)stuff.get(0)).getNamedCapture("value");
+						builder.addLexerRule(ruleName, regexValue);
+					} else {
+						throw new IllegalStateException("SEVERE: We should not be able to produce a nonterminal 'regexp' Production!");
+					}
+				} else {
+					//The rest of the rule MUST be ProductionRule nodes, which can be grouped up in a SequenceProductionRule.
+					ProductionRule[] subRules = new ProductionRule[rulePieces.size()];
+					for(int i=0; i<subRules.length; i++) {
+						subRules[i] = makeRule(rulePieces.get(i));
+					}
+					SequenceProductionRule productionRule = SequenceProductionRule.of(ruleName, subRules);
+					builder.add(productionRule);
+				}
+			}
+		}
+		
+		return builder.build();
 	}
 }
